aba96b2568471d5c676abe8538959c45b45d5d9a
Jordan <jordan.m.kidd@wmich.edu>
2014-03-04 00:38:17 -0500

    small fix

--- GoClasses.go
+++ GoClasses.go
@@ -5,49 +5,43 @@ import "fmt"
 /*  More GoLang Examples for my reference!
     This focuses on 'classes' / OOP
 */
-
 type Person struct {
     name string
     job string
     ssn int
-}
-//Adds methods to the objects
+} //Adds methods to the objects
 func (p *Person) SayName() {
-    fmt.Printf("My name is %v\n", p.name)
+    fmt.Printf("My name is %v.\n", p.name)
 }
-
+func (p *Person) SayJob() {
+    fmt.Printf("My job is %v.\n", p.job)
+}
 //Embedded type (anonomous fields):
-//Android "is a" Person
-//person Person would be "has a"
+//Android "is a" Person.  person Person would be "has a"
 type Android struct {
     Person //No var name!
     modelNum int
     modelName string
 }
-func (a *Android) SayModelName() {
-    fmt.Printf("My MoDeL nAmE iS: %v\n", a.modelName)
+func (a *Android) SayName() {
+    fmt.Printf("...MY NAME IS: %v.\n", a.Person.name)
 }
 
-
 func main() {
-
     //Base class
     jordan := new(Person)
     jordan.name = "Jordan"
-    jordan.job = "Software engineer"
+    jordan.job = "Software Engineer"
     jordan.ssn = 333224444
     jordan.SayName()
-
-    fmt.Printf("\n")
 
     //'Inherited' class with Person methods
     beepboop := new(Android)
     beepboop.modelName = "123-AB-v92.23"
     beepboop.modelNum = 12345
-    beepboop.Person.name = "Droidy" //
-    beepboop.Person.job = "RoboCop"
-    beepboop.name = "blah"
-    beepboop.Person.SayName()
-    beepboop.SayModelName()
+    beepboop.name = "ANDY v1.0"
+    beepboop.job = "RoboCop" //assigns to Person's job prop
 
+    beepboop.SayName()
+    beepboop.SayJob() //use of Person's method on Android object
 }
